7c569639b7c0cb4b8537141605ff31c6802d69bb
Marek Smolarczyk <mareksmoalrczyk33@gmail.com>
2017-12-13 18:29:07 +0100

    Pełna implementacja copy on write

--- keyed_queue.h
+++ keyed_queue.h
@@ -74,15 +74,17 @@ public:
     }
 
     void push(K const &key, V const &value) {
-        key_queue.push_back(key_value_pair(key, value));
-        auto it = iterators_map.find(key);
+        write_imminent();
 
-        if (it == iterators_map.end()) {
+        (*key_queue).push_back(key_value_pair(key, value));
+        auto it = (*iterators_map).find(key);
+
+        if (it == (*iterators_map).end()) {
             iterators_list_type iter_list;
-            iter_list.push_back((key_queue.rbegin()).base());
-            iterators_map.insert(std::make_pair(key, iter_list));
+            iter_list.push_back((((*key_queue)).rbegin()).base());
+           (*iterators_map).insert(std::make_pair(key, iter_list));
         } else {
-            (it->second).push_back((key_queue.rbegin()).base());
+            (it->second).push_back(((*key_queue).rbegin()).base());
         }
 
         queue_size++;
@@ -91,94 +93,108 @@ public:
     void pop() {
         if (empty()) throw lookup_error();
 
-        auto it_queue = key_queue.begin();
+        write_imminent();
+
+        auto it_queue = (*key_queue).begin();
         K key = it_queue->key;
-        auto it_map = iterators_map.find(key);
+        auto it_map = (*iterators_map).find(key);
 
         (it_map->second).pop_front();
-        if ((it_map->second).empty()) iterators_map.erase(key);
-        key_queue.pop_front();
+        if ((it_map->second).empty())(*iterators_map).erase(key);
+        (*key_queue).pop_front();
 
         queue_size--;
     }
 
     void pop(K const &key) {
-        auto it = iterators_map.find(key);
-        if (it == iterators_map.end()) throw lookup_error();
+        write_imminent();
 
-        key_queue.erase((it->second).front());
+        auto it = (*iterators_map).find(key);
+        if (it == (*iterators_map).end()) throw lookup_error();
+
+        (*key_queue).erase((it->second).front());
         (it->second).pop_front();
-        if ((it->second).empty()) iterators_map.erase(key);
+        if ((it->second).empty())(*iterators_map).erase(key);
 
         queue_size--;
     }
 
     void move_to_back(K const &key) {
-        auto it_map = iterators_map.find(key);
-        if (it_map == iterators_map.end()) throw lookup_error();
+        write_imminent();
+        
+        auto it_map = (*iterators_map).find(key);
+        if (it_map == (*iterators_map).end()) throw lookup_error();
 
         for (auto it_iter = (it_map->second).begin(); it_iter != (it_map->second).end(); ++it_iter) {
             key_value_pair moved_element(**it_iter);
-            key_queue.erase(*it_iter);
-            key_queue.push_back(moved_element);
+            (*key_queue).erase(*it_iter);
+            (*key_queue).push_back(moved_element);
         }
     }
 
     std::pair<K const &, V &> front() {
         if (empty()) throw lookup_error();
 
-        key_value_pair kv = key_queue.front();
+        write_imminent();
+
+        key_value_pair kv = (*key_queue).front();
         return {kv.key, kv.value};
     }
 
     std::pair<K const &, V &> back() {
         if (empty()) throw lookup_error();
 
-        key_value_pair kv = key_queue.back();
+        write_imminent();
+
+        key_value_pair kv = (*key_queue).back();
         return {kv.key, kv.value};
     }
 
     std::pair<K const &, V const &> front() const {
         if (empty()) throw lookup_error();
 
-        key_value_pair kv = key_queue.front();
+        key_value_pair kv = (*key_queue).front();
         return {kv.key, kv.value};
     }
 
     std::pair<K const &, V const &> back() const {
         if (empty()) throw lookup_error();
 
-        key_value_pair kv = key_queue.back();
+        key_value_pair kv = (*key_queue).back();
         return {kv.key, kv.value};
     }
 
     std::pair<K const &, V &> first(K const &key) {
-        auto it = iterators_map.find(key);
-        if (it == iterators_map.end()) throw lookup_error();
+        write_imminent();
+
+        auto it = (*iterators_map).find(key);
+        if (it == (*iterators_map).end()) throw lookup_error();
 
         key_value_pair kv = *(it->second.front());
         return {kv.key, kv.value};
     }
 
     std::pair<K const &, V &> last(K const &key) {
-        auto it = iterators_map.find(key);
-        if (it == iterators_map.end()) throw lookup_error();
+        write_imminent();
+        
+        auto it = (*iterators_map).find(key);
+        if (it == (*iterators_map).end()) throw lookup_error();
 
         key_value_pair kv = *(it->second.back());
         return {kv.key, kv.value};
     }
 
     std::pair<K const &, V const &> first(K const &key) const {
-        auto it = iterators_map.find(key);
-        if (it == iterators_map.end()) throw lookup_error();
+        auto it = (*iterators_map).find(key);
+        if (it == (*iterators_map).end()) throw lookup_error();
 
         key_value_pair kv = *(it->second.front());
         return {kv.key, kv.value};
     }
 
     std::pair<K const &, V const &> last(K const &key) const {
-        auto it = iterators_map.find(key);
-        if (it == iterators_map.end()) throw lookup_error();
+        auto it = (*iterators_map).find(key);
+        if (it == (*iterators_map).end()) throw lookup_error();
 
         key_value_pair kv = *(it->second.back());
         return {kv.key, kv.value};
@@ -193,13 +209,15 @@ public:
     }
 
     void clear() {
-        key_queue.clear();
-        iterators_map.clear();
+        write_imminent();
+
+        (*key_queue).clear();
+        (*iterators_map).clear();
         queue_size = 0;
     }
 
     size_t count(K const &key) const {
-        auto it = iterators_map.find(key);
+        auto it = (*iterators_map).find(key);
 
         return (it->second).size();
     }
@@ -227,11 +245,11 @@ public:
     };
 
     k_iterator k_begin() noexcept {
-        return k_iterator(iterators_map.begin());
+        return k_iterator((*iterators_map).begin());
     }
 
     k_iterator k_end() noexcept {
-        return k_iterator(iterators_map.end());
+        return k_iterator((*iterators_map).end());
     }
 
 };
